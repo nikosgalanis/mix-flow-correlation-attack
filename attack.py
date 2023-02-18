5debd8ff156ec067e363fd9b74c6af636fcf9e8a
Michael Belegris <b9015988@newcastle.ac.uk>
2023-02-18 11:56:03 +0000

    Created boilerplate functions for attack.py

---
+++ attack.py
@@ -0,0 +1,46 @@
+"""
+Step 1: Data Collection
+-Get input packets time of entering mix network
+-Get output packets time of arriving at node
+
+Step 2: Flow Pattern Extraction
+-Transform list of input packets and output packets into pattern vectors
+
+Step 3: Distance Function Selection
+-Determine the distance between the pattern vectors
+-Distance Function 1: Mutual Information
+-Distance Function 2: Frequency-spectrum-based matched filter
+
+Step 4: Flow Correlation
+-Selecting the OUTPUT link whose traffic has the minimum distance to INPUT flow pattern vector
+"""
+
+
+def dataCollection():
+    # TODO: Implement Data Collection
+    pass
+
+
+def flowPatternExtraction():
+    # TODO: Implement Flow Pattern Extraction
+    pass
+
+
+def dist_mutual_info():
+    # TODO: Implement Mutual Information distance function
+    pass
+
+
+def dist_fsb_matched_filter():
+    # TODO: Implement Frequency-Spectrum-Based matched filter distance function
+    pass
+
+
+def distanceFunctionSelection():
+    # TODO: Implement Distance Function Selection
+    pass
+
+
+def flowCorrelationAttack():
+    # TODO: Implement Attack
+    pass
